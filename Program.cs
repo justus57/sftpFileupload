6a4f0ac5ea3aef9825295c22da095d8d2b5802d4
justus7 <justuskasyoki57@gmail.com>
2021-12-08 11:28:01 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,86 @@
+﻿using Chilkat;
+using Renci.SshNet;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KCB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string source = @"Documents\credentials";
+            //string destination = @"C:/Users/CJ\Downloads/KCB/KCB PROJECT";
+            string destination = @"C:\\Downloads\KCB";
+            string host = "127.0.0.1";
+            string username = "justus";
+            string password = "Brightsoft@20";
+            string port = "22";  //Port 22 is defaulted for SFTP upload
+
+            SendPaymentFile(host, username, password, source, destination, port);
+
+            System.Console.WriteLine(source);
+            Console.ReadLine();
+            WriteLog(source);
+        }
+        //Parameters for sending Payment File
+        public static void SendPaymentFile(string host, string username, string password, string remoteFilePath, string localFilePath, string port)
+        {
+            try
+            {
+                using (ScpClient client = new ScpClient(host, password,port))
+                {
+                    client.Connect();
+
+                    using (System.IO.Stream localFile = File.OpenRead(localFilePath))
+                    {
+                        client.Upload(localFile, remoteFilePath);
+                    }
+
+                }
+                Console.WriteLine("Successfully connected to the client");
+                Console.ReadLine();
+            }
+            catch (Exception es)
+            {
+                WriteLog(es.Message);
+            }
+        }
+        public static void WriteLog(string text)
+        {
+
+            try
+            {
+                //set up a filestream
+                string strPath = @"C:\Logs\KCBAPI";
+                string fileName = DateTime.Now.ToString("MMddyyyy") + "_logs.txt";
+                string filenamePath = strPath + '\\' + fileName;
+                Directory.CreateDirectory(strPath);
+                FileStream fs = new FileStream(filenamePath, FileMode.OpenOrCreate, System.IO.FileAccess.Write);
+                //set up a streamwriter for adding text
+                StreamWriter sw = new StreamWriter(fs);
+                //find the end of the underlying filestream
+                sw.BaseStream.Seek(0, SeekOrigin.End);
+                //add the text
+                sw.WriteLine(DateTime.Now.ToString() + " : " + text);
+                //add the text to the underlying filestream
+                sw.Flush();
+                //close the writer
+                sw.Close();
+            }
+            catch (Exception ex)
+            {
+                //throw;
+                ex.Data.Clear();
+            }
+        }
+    }  
+}
+
+
+
+
